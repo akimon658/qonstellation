281d2cd48404bfa26656affab9ef91c0f87d9e16
Takumi Akimoto <noreply@akimo.dev>
2026-05-02 02:20:24 +0900

    Fix OAuth2 endpoints

--- lib/oauth2.ts
+++ lib/oauth2.ts
@@ -1,7 +1,9 @@
 import { OAuth2Client } from "@badgateway/oauth2-client"
 
 export const oauth2Client = new OAuth2Client({
-  server: "https://q.trap.jp/api/v3/oauth2",
+  server: "https://q.trap.jp",
   clientId: Deno.env.get("TRAQ_CLIENT_ID")!,
   clientSecret: Deno.env.get("TRAQ_CLIENT_SECRET")!,
+  tokenEndpoint: "/api/v3/oauth2/token",
+  authorizationEndpoint: "/api/v3/oauth2/authorize",
 })
